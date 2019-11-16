2bdc49f09ffcc772cff89f4d29df07a33e93ae58
lucasarruda <lucasarruda@gmail.com>
2019-11-16 20:27:52 -0300

    Create seed with out data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+dns_record_1 = DnsRecord.create(ip: '1.1.1.1', hostnames_attributes: [
+  {
+    'hostname': 'lorem.com'
+  },
+  {
+    'hostname': 'ipsum.com'
+  },
+  {
+    'hostname': 'dolor.com'
+  },
+  {
+    'hostname': 'amet.com'
+  }
+])
+
+dns_record_2 = DnsRecord.create(ip: '2.2.2.2', hostnames_attributes: [
+  {
+    'hostname': 'ipsum.com'
+  }
+])
+
+dns_record_3 = DnsRecord.create(ip: '3.3.3.3', hostnames_attributes: [
+  {
+    'hostname': 'ipsum.com'
+  },
+  {
+    'hostname': 'dolor.com'
+  },
+  {
+    'hostname': 'amet.com'
+  }
+])
+
+dns_record_4 = DnsRecord.create(ip: '4.4.4.4', hostnames_attributes: [
+  {
+    'hostname': 'ipsum.com'
+  },
+  {
+    'hostname': 'dolor.com'
+  },
+  {
+    'hostname': 'sit.com'
+  },
+  {
+    'hostname': 'amet.com'
+  }
+])
+
+dns_record_5 = DnsRecord.create(ip: '5.5.5.5', hostnames_attributes: [
+  {
+    'hostname': 'dolor.com'
+  },
+  {
+    'hostname': 'sit.com'
+  }
+])
