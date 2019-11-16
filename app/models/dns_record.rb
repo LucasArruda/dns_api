3c8e1b0387068f6cfafdf93df07783f353050547
lucasarruda <lucasarruda@gmail.com>
2019-11-16 20:28:40 -0300

    Accept ip alias instead of ip_address when saving new record

--- app/models/dns_record.rb
+++ app/models/dns_record.rb
@@ -3,4 +3,8 @@ class DnsRecord < ApplicationRecord
   accepts_nested_attributes_for :hostnames
 
   validates :ip_address, presence: true
+
+  def ip=(ip_address)
+    self.ip_address = ip_address
+  end
 end
