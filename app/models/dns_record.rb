d96497a081e6ccabb75e5ef3ab709761946ef5fc
lucasarruda <lucasarruda@gmail.com>
2019-11-16 19:29:57 -0300

    DNS Records accept nested attributes for hostnames

--- app/models/dns_record.rb
+++ app/models/dns_record.rb
@@ -1,4 +1,6 @@
 class DnsRecord < ApplicationRecord
   has_many :hostnames
+  accepts_nested_attributes_for :hostnames
+
   validates :ip_address, presence: true
 end
--- test/models/dns_record_test.rb
+++ test/models/dns_record_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class DnsRecordTest < ActiveSupport::TestCase
   context 'associations' do
     should have_many(:hostnames)
+
+    should accept_nested_attributes_for(:hostnames)
   end
 
   context 'validations' do
