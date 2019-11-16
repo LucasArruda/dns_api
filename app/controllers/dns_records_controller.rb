5def01afee7d41c9490e611cb7de971180e2a2f6
lucasarruda <lucasarruda@gmail.com>
2019-11-16 20:45:17 -0300

    Tests for record creation and response

--- app/controllers/dns_records_controller.rb
+++ app/controllers/dns_records_controller.rb
@@ -4,3 +4,18 @@ class DnsRecordsController < ApplicationController
 
     render json: @dns_records
   end
+  def create
+    @dns_record = DnsRecord.new(dns_record_params)
+
+    if @dns_record.save
+      render json: { id: @dns_record.id }, status: :created
+    else
+      render json: @dns_record.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def dns_record_params
+      params.require(:dns_record).permit(:ip, :ip_address, hostnames_attributes: {})
+    end
+end
--- test/controllers/dns_records_controller_test.rb
+++ test/controllers/dns_records_controller_test.rb
@@ -9,4 +9,32 @@ class DnsRecordsControllerTest < ActionDispatch::IntegrationTest
     get dns_records_url, as: :json
     assert_response :success
   end
+
+  test "should create dns_record" do
+    assert_difference('DnsRecord.count') do
+      post dns_records_url, params: {
+          dns_record: {
+            ip: @dns_record.ip_address,
+            hostnames_attributes: {
+
+            }
+          }
+        }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "created dns_record has id returned" do
+    post dns_records_url, params: {
+        dns_record: {
+          ip: @dns_record.ip_address,
+          hostnames_attributes: {
+
+          }
+        }
+      }, as: :json
+
+    assert @response.body['id'].present?
+  end
 end
